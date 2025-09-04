dfba279383e078a0f840042fa3b125418569deb9
alexbleich <alexbleich03@gmail.com>
2025-09-04 11:06:32 -0400

    Update housing-affordability-tool.py
    added bar

--- housing-affordability-tool.py
+++ housing-affordability-tool.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter, MaxNLocator
 from dataclasses import dataclass
 
+
 # ===== Constants & Files =====
 @dataclass(frozen=True)
 class Paths:
